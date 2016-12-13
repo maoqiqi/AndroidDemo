93cab99bd303b4b416edc489016bd6a83593644f
maoqiqi <15221394017@163.com>
2016-12-13 23:06:47 +0800

    modify theme styles

---
+++ app/src/main/java/com/software/march/utils/Constant.java
@@ -0,0 +1,16 @@
+package com.software.march.utils;
+
+import com.software.march.R;
+
+/**
+ * @author Doc.March
+ * @version V 1.0
+ * @Description
+ * @date 2016/12/13
+ */
+public class Constant {
+
+    public static final int RESOURCES_DAYTHEME = R.style.ThemeAppDay;
+    public static final int RESOURCES_NIGHTTHEME = R.style.ThemeAppNight;
+
+}
