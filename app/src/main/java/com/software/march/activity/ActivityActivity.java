5e2ae20235432f4203fce0145b5d08866b3d3c8e
maoqiqi <15221394017@163.com>
2016-12-16 11:27:46 +0800

    Activity start mode,and button click events.

--- app/src/main/java/com/software/march/activity/ActivityActivity.java
+++ app/src/main/java/com/software/march/activity/ActivityActivity.java
@@ -22,20 +22,35 @@ public class ActivityActivity extends AppCompatActivity implements View.OnClickL
 
     private Button btnLifecycle;
     private Button btnRestoreInstance;
+    private Button btnStartMode;
 
+    /**
+     * 重写的方法：onCreate(Bundle savedInstanceState)
+     * 在当前类(activity)对象创建的时候自动调用。
+     * 回调方法:不是我们调的,是系统在一定条件下自动调用的,基本都以on开头onXXX,这些方法我们不需要调用,一般只是去重写此类方法.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // 调用父类处理做一些默认的初始化工作
         super.onCreate(savedInstanceState);
+        // 动态设置主题
         setTheme(SPUtils.getThemeRes(this));
+        // 加载布局文件,并在内存中生成对应的视图对象
         setContentView(R.layout.activity_activity);
 
+        // 根据id在内存查找得到对应的视图对象
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // 初始化视图对象
         btnLifecycle = (Button) findViewById(R.id.btn_lifecycle);
         btnRestoreInstance = (Button) findViewById(R.id.btn_restore_instance);
+        btnStartMode = (Button) findViewById(R.id.btn_start_mode);
+
+        // 设置点击监听
         btnLifecycle.setOnClickListener(this);
         btnRestoreInstance.setOnClickListener(this);
+        btnStartMode.setOnClickListener(this);
     }
 
     @Override
@@ -46,6 +61,9 @@ public class ActivityActivity extends AppCompatActivity implements View.OnClickL
                 break;
             case R.id.btn_restore_instance:
                 startActivity(new Intent(this, RestoreInstanceActivity.class));
+                break;
+            case R.id.btn_start_mode:
+                startActivity(new Intent(this, StartModeActivity.class));
                 break;
         }
     }
--- app/src/main/java/com/software/march/activity/StartModeActivity.java
+++ app/src/main/java/com/software/march/activity/StartModeActivity.java
@@ -2,14 +2,26 @@ package com.software.march.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.software.march.R;
+import com.software.march.utils.SPUtils;
 
+/**
+ * @author Doc.March
+ * @version V 1.0
+ * @Description Activity启动方式
+ * @date 2016/12/16
+ */
 public class StartModeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(SPUtils.getThemeRes(this));
         setContentView(R.layout.activity_start_mode);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
