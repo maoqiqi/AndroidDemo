c9dd9f3e12f43210f253431e3b1ff0760d6b89f5
maoqiqi <15221394017@163.com>
2016-12-28 16:15:52 +0800

    Update network data storage.

--- app/src/main/java/com/software/march/activity/NetWorkActivity.java
+++ app/src/main/java/com/software/march/activity/NetWorkActivity.java
@@ -1,13 +1,27 @@
 package com.software.march.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.software.march.R;
+import com.software.march.utils.AppUtils;
 import com.software.march.utils.SPUtils;
 
-public class NetWorkActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetWorkActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnDownloadApk;
+    private File apkFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +31,80 @@ public class NetWorkActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        btnDownloadApk = (Button) findViewById(R.id.btn_download_apk);
+        btnDownloadApk.setOnClickListener(this);
+
+        // 准备用于保存APK文件的File对象:/storage/sdcard/Android/package_name/files/xxx.apk
+        apkFile = new File(getExternalFilesDir(null), "update.apk");
+    }
+
+    @Override
+    public void onClick(View view) {
+        downloadApk();
+    }
+
+    private void downloadApk() {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        dialog.show();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    String path = "http://112.65.235.163/imtt.dd.qq.com/16891/6A4A3337FC49B1162C4CD0D239AD7C7D.apk?mkey=586341779ebbd0cf&f=d388&c=0&fsname=com.vteam.cook_3.1.1_3110.apk&csr=4d5s&p=.apk";
+                    // 创建URL对象
+                    URL url = new URL(path);
+                    // 打开连接,得到HttpURLConnection对象
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    // 设置请求方式,连接超时,读取数据超时
+                    conn.setRequestMethod("POST");
+                    conn.setConnectTimeout(5000);
+                    conn.setReadTimeout(6000);
+                    // 连接服务器
+                    conn.connect();
+                    // 发请求,得到响应数据
+                    // 得到响应码,必须是200才读取
+                    if (conn.getResponseCode() == 200) {
+                        // 设置dialog的最大进度
+                        dialog.setMax(conn.getContentLength());
+
+                        // 得到InputStream,并读取成String
+                        InputStream is = conn.getInputStream();
+                        // 创建指向apkFile的FileOutputStream
+                        FileOutputStream fos = new FileOutputStream(apkFile);
+                        byte[] buffer = new byte[1024];
+                        int len = -1;
+                        while ((len = is.read(buffer)) != -1) {
+                            fos.write(buffer, 0, len);
+                            // 显示下载进度
+                            dialog.incrementProgressBy(len);
+
+                            // 休息一会(模拟网速慢)
+                            // Thread.sleep(50);
+                            // SystemClock.sleep(50);
+                        }
+
+                        fos.close();
+                        is.close();
+                    }
+                    // 断开连接
+                    conn.disconnect();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            dialog.dismiss();
+                            AppUtils.installApp(NetWorkActivity.this, apkFile);
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
--- app/src/main/java/com/software/march/utils/AppUtils.java
+++ app/src/main/java/com/software/march/utils/AppUtils.java
@@ -3,22 +3,164 @@ package com.software.march.utils;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.telephony.SmsManager;
 
 import com.software.march.bean.AppInfoBean;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author Doc.March
  * @version V 1.0
- * @Description
- * @date 2016/12/21
+ * @Description Helper class for App
+ * @date 2016/11/11
  */
 public class AppUtils {
+
+    private AppUtils() {
+        /* cannot be instantiated */
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    /**
+     * Gets the current version code of the application
+     *
+     * @param context
+     * @return version code
+     */
+    public static int getVersionCode(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * Gets the current version name of the application
+     *
+     * @param context
+     * @return version name
+     */
+    public static String getVersionName(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Gets the current name for the application
+     *
+     * @param context
+     * @return app name
+     */
+    public static String getAppName(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            int labelRes = packageInfo.applicationInfo.labelRes;
+            return context.getResources().getString(labelRes);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // 1.创建一个Intent(隐式)
+    // 2.携带数据
+    // 3.startActivity(intent)
+
+    /**
+     * install app
+     *
+     * @param context
+     * @param file
+     */
+    public static void installApp(Context context, File file) {
+        // Intent intent = new Intent(Intent.ACTION_VIEW);
+        // //09-05 12:59:20.553: I/ActivityManager(1179): Displayed com.android.packageinstaller/.PackageInstallerActivity: +282ms
+        Intent intent = new Intent(Intent.ACTION_INSTALL_PACKAGE);
+        intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+        context.startActivity(intent);
+    }
+
+    /**
+     * 调起系统所有的播放 - 隐式意图
+     *
+     * @param context
+     * @param path
+     */
+    public static void playVideo(Context context, String path) {
+        Intent intent = new Intent();
+        intent.setDataAndType(Uri.parse(path), "video/*");
+        context.startActivity(intent);
+    }
+
+    /**
+     * 进入拨号界面
+     *
+     * @param context
+     */
+    public static void dial(Context context, String number) {
+        // 08-28 03:27:09.976: I/ActivityManager(1222): Displayed com.android.dialer/.DialtactsActivity: +535ms
+        String action = Intent.ACTION_DIAL;// android.intent.action.DIAL
+        Intent intent = new Intent(action);
+        intent.setData(Uri.parse("tel:" + number)); // <data android:scheme="tel" />
+        context.startActivity(intent);
+    }
+
+    /**
+     * 直接拨打电话
+     * <uses-permission android:name="android.permission.CALL_PHONE"/>
+     *
+     * @param context
+     */
+    private void call(Context context, String number) {
+        // 08-28 03:38:59.717: I/ActivityManager(1222): Displayed com.android.phone/.PrivilegedOutgoingCallBroadcaster: +570ms
+        Intent intent = new Intent(Intent.ACTION_CALL);// android.intent.action.CALL
+        intent.setData(Uri.parse("tel:" + number));
+        // context.startActivity(intent);
+    }
+
+    /**
+     * 进入编辑短信界面
+     *
+     * @param context
+     */
+    private void sendto(Context context, String number, String sms) {
+        // 08-28 04:00:02.420: I/ActivityManager(1222): Displayed com.android.mms/.ui.ComposeMessageActivity: +132ms
+        Intent intent = new Intent(Intent.ACTION_SENDTO);// android.intent.action.SENDTO
+        intent.setData(Uri.parse("smsto:" + number));
+        // 携带额外数据
+        intent.putExtra("sms_body", sms);
+        context.startActivity(intent);
+    }
+
+    /**
+     * 直接将短信发送出去
+     */
+    private void sendMessage(String number, String sms) {
+        // 1.得到SmsManager的对象
+        SmsManager smsManager = SmsManager.getDefault();
+        // 2.发送文本信息(短信)
+        smsManager.sendTextMessage(number, null, sms, null, null);
+    }
+
 
     /**
      * 判断某个应用程序是不是三方的应用程序
